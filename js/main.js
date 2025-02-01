30a8a959bfb77bcd3b22947ef1721776d767ed40
Lounger <4087076+TheLounger@users.noreply.github.com>
2025-02-01 01:09:34 +0100

    Initial

--- js/main.js
+++ js/main.js
@@ -238,7 +238,7 @@ for (i = 0; i < slimDropdownElements.length; i++) {
 // The show/hide events were adapted from:
 // https://github.com/SillyTavern/SillyTavern/blob/6c8bd06308c69d51e2eb174541792a870a83d2d6/public/script.js
 //------------------------------------------------
-var buttonsInChat = document.querySelectorAll("#chat-tab #chat-buttons button");
+var buttonsInChat = document.querySelectorAll("#chat-tab:not(.chat-buttons-inline) #chat-buttons button");
 var button = document.getElementById("hover-element-button");
 var menu = document.getElementById("hover-menu");
 var istouchscreen = (navigator.maxTouchPoints > 0) || "ontouchstart" in document.documentElement;
@@ -273,6 +273,13 @@ if (buttonsInChat.length > 0) {
       thisButton.innerHTML = newText;
     }
   }
+} else {
+    buttonsInChat = document.querySelectorAll("#chat-tab.chat-buttons-inline #chat-buttons button");
+    for (let i = 0; i < buttonsInChat.length; i++) {
+        buttonsInChat[i].textContent = buttonsInChat[i].textContent.replace(/ \(.*?\)/, "");
+    }
+    document.getElementById("chat-col").appendChild(document.getElementById("chat-buttons"));
+    document.getElementById("gr-hover-container").style.display = "none";
 }
 
 function isMouseOverButtonOrMenu() {
--- modules/shared.py
+++ modules/shared.py
@@ -82,6 +82,7 @@ group.add_argument('--model-menu', action='store_true', help='Show a model menu 
 group.add_argument('--settings', type=str, help='Load the default interface settings from this yaml file. See settings-template.yaml for an example. If you create a file called settings.yaml, this file will be loaded by default without the need to use the --settings flag.')
 group.add_argument('--extensions', type=str, nargs='+', help='The list of extensions to load. If you want to load more than one extension, write the names separated by spaces.')
 group.add_argument('--verbose', action='store_true', help='Print the prompts to the terminal.')
+group.add_argument('--chat-buttons', action='store_true', help='Show buttons on the chat tab instead of a hover menu.')
 group.add_argument('--idle-timeout', type=int, default=0, help='Unload model after this many minutes of inactivity. It will be automatically reloaded when you try to use it again.')
 
 # Model loader
@@ -205,7 +206,6 @@ group.add_argument('--multimodal-pipeline', type=str, default=None, help='The mu
 group = parser.add_argument_group('Deprecated')
 group.add_argument('--cache_4bit', action='store_true', help='DEPRECATED')
 group.add_argument('--cache_8bit', action='store_true', help='DEPRECATED')
-group.add_argument('--chat-buttons', action='store_true', help='DEPRECATED')
 group.add_argument('--triton', action='store_true', help='DEPRECATED')
 group.add_argument('--no_inject_fused_mlp', action='store_true', help='DEPRECATED')
 group.add_argument('--no_use_cuda_fp16', action='store_true', help='DEPRECATED')
@@ -226,7 +226,6 @@ for arg in sys.argv[1:]:
 deprecated_args = [
     'cache_4bit',
     'cache_8bit',
-    'chat_buttons',
     'triton',
     'no_inject_fused_mlp',
     'no_use_cuda_fp16',
